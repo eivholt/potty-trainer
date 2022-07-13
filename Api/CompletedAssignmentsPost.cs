624e7ce6d5e95d7bd09da2323b79e9cb26153dba
Eivind Holt <eivholt@gmail.com>
2022-07-13 18:19:26 +0200

    completedassignments api

--- Api/CompletedAssignmentsPost.cs
+++ Api/CompletedAssignmentsPost.cs
@@ -23,13 +23,21 @@ namespace Api
 
         [FunctionName("CompletedAssignments")]
         public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "users/{userid:int}/completedassignments/{assignmentid:int}")] HttpRequest req,
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "users/{userid:int}/completedassignments")] HttpRequest req,
             ILogger log,
-            int userid,
-            int assignmentid)
+            int userid)
         {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic completedAssignment = JsonConvert.DeserializeObject(requestBody);
+            
+
             var user = await m_userData.GetUserWithAssignments(userid);
-            user.CompletedAssignments.Add(new CompletedAssignment { Timestamp = DateTime.Now, Assignment = user.Assignments.First(assignment => assignment.Id.Equals(assignmentid)) });
+            user.CompletedAssignments.Add(
+                new CompletedAssignment 
+                { 
+                    Timestamp = DateTime.Now, 
+                    Assignment = user.Assignments.First(assignment => assignment.Id.Equals((int)completedAssignment.id)) 
+                });
 
             return new OkObjectResult(user);
         }
